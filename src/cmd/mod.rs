36b03b3264b8c3e3e45f8fa0cb47b39d056a6159
Eguo Wang <wangeguo@gmail.com>
2022-04-27 13:28:30 +0800

    Added amp fix command and options

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -454,8 +454,25 @@ pub fn build() -> Command<'static> {
             ])
             .after_help("Use \"amp options\" for a list of global command-line options (applies to all commands).")
         )
-        .subcommand(Command::new("fix").about("Update old configuration to a newer schema version"))
-
+        .subcommand(Command::new("fix")
+            .about("Update old configuration to a newer schema version")
+            .args(&[
+                Arg::new("assume-yes").long("assume-yes").takes_value(false).help("If true, amp will skip yes/no confirmation from the user and default to yes"),
+                Arg::new("filename").short('f').long("filename").default_value(".amp.yaml").help("Path or URL to the Amphitheatre config file"),
+                Arg::new("module").short('m').long("module").default_value("[]").help("Filter Amphitheatre configs to only the provided named modules"),
+                Arg::new("profile").short('p').long("profile").default_value("[]").help("Activate profiles by name (prefixed with `-` to disable a profile)"),
+                Arg::new("output").short('o').long("output").takes_value(true).help("File to write the changed config (instead of standard output)"),
+                Arg::new("overwrite").long("overwrite").takes_value(false).help("Overwrite original config with fixed config"),
+                Arg::new("remote-cache-dir").long("remote-cache-dir").default_value("$HOME/.amp/repos").help("Specify the location of the git repositories cache"),
+                Arg::new("sync-remote-cache").long("sync-remote-cache").default_value("missing")
+                .help("Controls how Amphitheatre manages the remote config cache (see `remote-cache-dir`). \
+                        One of `always` (default), `missing`, or `never`. `always` syncs remote repositories \
+                        to latest on access. `missing` only clones remote repositories if they do not exist \
+                        locally. `never` means the user takes responsibility for updating remote repositories."),
+                Arg::new("version").long("version").default_value("amp/v1").help("Target schema version to upgrade to")
+            ])
+            .after_help("Use \"amp options\" for a list of global command-line options (applies to all commands).")
+        )
         .subcommand(Command::new("init")
             .about("Generate configuration for deploying an application")
             .args(&[
