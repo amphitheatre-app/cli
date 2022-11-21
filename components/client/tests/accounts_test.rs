3f485c6b519213f95cf853a20bd2022cda63aa43
Eguo Wang <wangeguo@gmail.com>
2022-11-21 17:30:22 +0800

    Moved the fixture `me/success` to `accounts/get_me_success`

--- components/client/tests/accounts_test.rs
+++ components/client/tests/accounts_test.rs
@@ -18,7 +18,7 @@ mod common;
 
 #[test]
 fn me_success_with_account() {
-    let setup = setup_mock_for("/me", "me/success", "GET");
+    let setup = setup_mock_for("/me", "accounts/get_me_success", "GET");
     let client = setup.0;
     let response = client.accounts().me().unwrap();
 
