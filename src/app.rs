975f22cce943c0705ac7e4df06f94e2f37bb8722
Eguo Wang <wangeguo@gmail.com>
2022-11-23 01:40:07 +0800

    allowd verbosity dead_code (used in macros), and updated expect messages

--- src/app.rs
+++ src/app.rs
@@ -17,10 +17,11 @@ use once_cell::sync::OnceCell;
 
 static VERBOSITY: OnceCell<LevelFilter> = OnceCell::new();
 
+#[allow(dead_code)]
 pub fn verbosity() -> &'static LevelFilter {
-    VERBOSITY.get().expect("verbosity is not initialized")
+    VERBOSITY.get().expect("Verbosity is not initialized")
 }
 
 pub fn set_global_verbosity(verbosity: LevelFilter) {
-    VERBOSITY.set(verbosity).expect("could not set verbosity")
+    VERBOSITY.set(verbosity).expect("Could not set verbosity")
 }
