93f4bfd3ced9c08435d01fd8e0c6d4af50f8cdd2
Eguo Wang <wangeguo@gmail.com>
2023-07-17 21:09:28 +0800

    Continuous incremental synchronization of local code to the server #5

--- src/ops/dev.rs
+++ src/ops/dev.rs
@@ -15,18 +15,19 @@
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
+use amp_client::actors::{Actors, SynchronizationRequest};
 use amp_client::client::Client;
 use amp_client::playbooks::PlaybookPayload;
 use amp_common::filesystem::Finder;
-use amp_common::schema::EitherCharacter;
+use amp_common::schema::{EitherCharacter, Manifest};
 use ignore::gitignore::{Gitignore, GitignoreBuilder};
 use ignore::WalkBuilder;
 use notify::event::{CreateKind, DataChange, ModifyKind, RemoveKind};
-use notify::EventKind::{Create, Modify, Remove};
+use notify::EventKind::{self, Create, Modify, Remove};
 use notify::RecursiveMode::Recursive;
-use notify::{RecommendedWatcher, Watcher};
+use notify::{Event, RecommendedWatcher, Watcher};
 use tar::Builder;
-use tracing::{debug, error, info, trace};
+use tracing::{debug, error, info, trace, warn};
 
 use crate::context::Context;
 use crate::errors::{Errors, Result};
@@ -41,7 +42,7 @@ pub async fn dev(ctx: Arc<Context>) -> Result<()> {
     let payload = PlaybookPayload {
         title: "Untitled".to_string(),
         description: "".to_string(),
-        preface: EitherCharacter::Manifest(content),
+        preface: EitherCharacter::Manifest(content.clone()),
         live: true,
     };
     debug!("{:#?}", payload);
@@ -53,14 +54,16 @@ pub async fn dev(ctx: Arc<Context>) -> Result<()> {
     info!("The playbook was created and deployed successfully!");
     debug!("{:#?}", playbook);
 
-    let bytes = archive(workspace).unwrap();
-    debug!("Archived all codes: {}", bytes.len());
-    // let manifest: Manifest = toml::from_str(&content).map_err(Errors::InvalidManifest)?;
-    // client
-    //     .actors()
-    //     .sync(playbook.id, manifest.name, bytes)
-    //     .map_err(Errors::ClientError)?;
+    // Continuous Synchornize file changes.
+    // first time, we need to sync all files.
+    // and then, we need to sync only changed files.
+    let actors = client.actors();
+    let manifest: Manifest = toml::from_str(&content).map_err(Errors::InvalidManifest)?;
 
+    // Initial sync the full sources into the server.
+    upload(&actors, &playbook.id, &manifest.name, workspace)?;
+
+    // Watch file changes and sync the changed files.
     let (tx, rx) = std::sync::mpsc::channel();
     // We listen to the file changes giving Notify
     // a function that will get called when events happen.
@@ -74,29 +77,93 @@ pub async fn dev(ctx: Arc<Context>) -> Result<()> {
     let matcher = builder.build().unwrap();
 
     for event in rx {
-        match event {
-            Ok(event) => {
-                trace!("Changed: {:?}", event);
-                // go through if the file is ignored
-                if is_ignored(&matcher, workspace, &event.paths)? {
-                    continue;
-                }
-
-                match event.kind {
-                    Create(CreateKind::File) | Create(CreateKind::Folder) => debug!("File created {:?}", event.paths),
-                    Modify(ModifyKind::Data(DataChange::Content)) => debug!("File modified {:?}", event.paths),
-                    Modify(ModifyKind::Name(mode)) => debug!("File renamed ({:?}) {:?}", mode, event.paths),
-                    Remove(RemoveKind::File) | Remove(RemoveKind::Folder) => {
-                        debug!("File removed {:?}", event.paths)
-                    }
-                    _ => {}
-                }
-            }
-            Err(err) => error!("Error: {err:?}"),
+        if let Err(err) = event {
+            error!("Got a notify error: {err:?}");
+            continue;
         }
+        let event = event.unwrap();
+        if is_ignored(&matcher, workspace, &event.paths)? {
+            continue;
+        }
+
+        handle(&actors, &playbook.id, &manifest.name, workspace, event)?;
     }
 
     Ok(())
+}
+
+fn upload(client: &Actors, pid: &str, name: &str, workspace: &Path) -> Result<()> {
+    let mut paths: Vec<(PathBuf, PathBuf)> = vec![];
+
+    let base = workspace;
+    for entry in WalkBuilder::new(workspace).build() {
+        let entry = entry.map_err(Errors::WalkError)?;
+        let path = entry.path();
+
+        if path.is_dir() {
+            continue;
+        }
+
+        paths.push(strip(base, path)?);
+    }
+
+    let pyaload = archive(&paths).unwrap();
+    let req = SynchronizationRequest {
+        kind: EventKinds::Overwrite.to_string(),
+        paths: vec![],
+        attributes: None,
+        payload: Some(pyaload),
+    };
+    client.sync(pid, name, req).map_err(Errors::ClientError)?;
+
+    Ok(())
+}
+
+fn handle(client: &Actors, pid: &str, name: &str, base: &Path, event: Event) -> Result<()> {
+    trace!("Changed: {:?}", event);
+
+    let kind = EventKinds::from(event.kind);
+    if kind == EventKinds::Rename || kind == EventKinds::Other {
+        warn!("Not supported event: {:?}", event);
+        return Ok(());
+    }
+
+    let mut paths: Vec<(PathBuf, PathBuf)> = vec![];
+    for path in event.paths {
+        paths.push(strip(base, &path)?);
+    }
+
+    let mut req = SynchronizationRequest {
+        kind: kind.to_string(),
+        paths: paths.iter().map(|(_, a)| a.to_str().unwrap().to_string()).collect(),
+        attributes: None,
+        payload: None,
+    };
+
+    if kind == EventKinds::Create || kind == EventKinds::Modify {
+        req.payload = Some(archive(&paths)?);
+    }
+
+    client.sync(pid, name, req).map_err(Errors::ClientError)?;
+    Ok(())
+}
+
+/// Archive the given directory into a tarball and return the bytes.
+fn archive(paths: &Vec<(PathBuf, PathBuf)>) -> Result<Vec<u8>> {
+    debug!("The given path is {:?}", paths);
+    let mut tar = Builder::new(Vec::new());
+    for (path, name) in paths {
+        tar.append_path_with_name(path, name)
+            .map_err(Errors::FailedAppendPath)?;
+    }
+    tar.into_inner().map_err(Errors::FailedFinishTar)
+}
+
+#[inline]
+fn strip(base: &Path, path: &Path) -> Result<(PathBuf, PathBuf)> {
+    let striped_path = path.strip_prefix(base).map_err(Errors::FailedStripPrefix)?;
+    debug!("the full path and striped path is: {:?}, {:?}", path, striped_path);
+    Ok((path.to_path_buf(), striped_path.to_path_buf()))
 }
 
 fn is_ignored(matcher: &Gitignore, root: &Path, paths: &Vec<PathBuf>) -> Result<bool> {
@@ -107,30 +174,40 @@ fn is_ignored(matcher: &Gitignore, root: &Path, paths: &Vec<PathBuf>) -> Result<
             return Ok(true);
         }
     }
-
     Ok(false)
 }
 
-#[allow(dead_code)]
-/// Archive the given directory into a tarball and return the bytes.
-fn archive(path: &Path) -> Result<Vec<u8>> {
-    debug!("The given path is {:?}", path);
-    let mut tar = Builder::new(Vec::new());
+#[derive(PartialEq, Eq)]
+enum EventKinds {
+    Overwrite,
+    Create,
+    Modify,
+    Rename,
+    Remove,
+    Other,
+}
 
-    let base = path;
-    for entry in WalkBuilder::new(path).build() {
-        let entry = entry.map_err(Errors::WalkError)?;
-        let path = entry.path();
-
-        if path.is_dir() {
-            continue;
+impl From<EventKind> for EventKinds {
+    fn from(kind: EventKind) -> Self {
+        match kind {
+            Create(CreateKind::File) | Create(CreateKind::Folder) => Self::Create,
+            Modify(ModifyKind::Data(DataChange::Content)) => Self::Modify,
+            Modify(ModifyKind::Name(_)) => Self::Rename,
+            Remove(RemoveKind::File) | Remove(RemoveKind::Folder) => Self::Remove,
+            _ => Self::Other,
         }
-
-        debug!("append path: {:?}, {:?}", path, path.to_path_buf());
-        let name = path.strip_prefix(base).map_err(Errors::FailedStripPrefix)?;
-        debug!("Striped path is {:?}", name);
-        tar.append_path(name).map_err(Errors::FailedAppendPath)?;
     }
+}
 
-    tar.into_inner().map_err(Errors::FailedFinishTar)
+impl ToString for EventKinds {
+    fn to_string(&self) -> String {
+        match self {
+            Self::Overwrite => "Overwrite".to_string(),
+            Self::Create => "Create".to_string(),
+            Self::Modify => "Modify".to_string(),
+            Self::Rename => "Rename".to_string(),
+            Self::Remove => "Remove".to_string(),
+            Self::Other => "Other".to_string(),
+        }
+    }
 }
