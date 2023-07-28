71c1a2aaeaefea8f3583f8b45ffd04b4c2fe69d0
Eguo Wang <wangeguo@gmail.com>
2023-07-28 15:36:53 +0800

    Refactor `amp context delete`: display a contexts list when not given name

--- src/cmd/context/delete.rs
+++ src/cmd/context/delete.rs
@@ -12,10 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::fmt::Display;
 use std::sync::Arc;
 
 use amp_common::config::Configuration;
 use clap::Args;
+use inquire::Select;
 
 use crate::context::Context;
 use crate::errors::{Errors, Result};
@@ -24,20 +26,61 @@ use crate::errors::{Errors, Result};
 #[derive(Args, Debug)]
 #[command(after_help = crate::cmd::cli::AFTER_HELP_STRING)]
 pub struct Cli {
-    name: String,
+    /// The name of the context to delete
+    name: Option<String>,
 }
 
 impl Cli {
     // delete the context and save the contexts
     pub async fn exec(&self, ctx: Arc<Context>) -> Result<()> {
-        let mut configuration = ctx.configuration.write().await;
-        let context = configuration.context.as_mut().ok_or(Errors::NotFoundContexts)?;
+        if let Some(name) = &self.name {
+            return delete(&ctx, name).await;
+        }
 
-        context.delete(&self.name).map_err(Errors::FailedDeleteContext)?;
-        configuration
-            .save(Configuration::path().map_err(Errors::InvalidConfigPath)?)
-            .map_err(Errors::FailedSaveConfiguration)?;
+        // display the available contexts for selection
+        let answer = select_context(&ctx).await?;
+        delete(&ctx, answer.0.as_str()).await?;
 
         Ok(())
     }
+}
+
+/// Select the context with the given name
+async fn select_context(ctx: &Arc<Context>) -> Result<OptionItem> {
+    let configuration = ctx.configuration.read().await;
+    let context = configuration.context.as_ref().ok_or(Errors::NotFoundContexts)?;
+
+    // create a options with the available contexts
+    let options: Vec<OptionItem> = context
+        .iter()
+        .map(|(name, ctx)| OptionItem(String::from(name), ctx.title.clone()))
+        .collect();
+
+    // run the select prompt
+    let answer = Select::new("Select the context:", options)
+        .prompt()
+        .map_err(Errors::InquireError)?;
+
+    Ok(answer)
+}
+
+#[derive(PartialEq)]
+struct OptionItem(String, String);
+
+impl Display for OptionItem {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.1)
+    }
+}
+
+async fn delete(ctx: &Arc<Context>, name: &str) -> Result<()> {
+    let mut configuration = ctx.configuration.write().await;
+    let context = configuration.context.as_mut().ok_or(Errors::NotFoundContexts)?;
+
+    context.delete(name).map_err(Errors::FailedDeleteContext)?;
+    configuration
+        .save(Configuration::path().map_err(Errors::InvalidConfigPath)?)
+        .map_err(Errors::FailedSaveConfiguration)?;
+
+    Ok(())
 }
